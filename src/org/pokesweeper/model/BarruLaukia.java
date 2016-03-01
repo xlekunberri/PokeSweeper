1f5b7694942384e8eceae3df38f7d3dc08951758
jajasuperman <enekosampedro@gmail.com>
2016-03-01 13:10:26 +0100

    BarruLaukia klasea eginda

--- src/org/pokesweeper/model/BarruLaukia.java
+++ src/org/pokesweeper/model/BarruLaukia.java
@@ -1,5 +1,37 @@
 package org.pokesweeper.model;
 
 public class BarruLaukia {
+	
+	private boolean mina;
+	private boolean begiratuta;
+	private int egoera;
+	
+	public BarruLaukia() {}
+
+	public boolean isMina() {
+		return mina;
+	}
+
+	public void setMina(boolean mina) {
+		this.mina = mina;
+	}
+
+	public boolean isBegiratuta() {
+		return begiratuta;
+	}
+
+	public void setBegiratuta(boolean begiratuta) {
+		this.begiratuta = begiratuta;
+	}
+
+	public int getEgoera() {
+		return egoera;
+	}
+
+	public void setEgoera(int egoera) {
+		this.egoera = egoera;
+	}
+	
+	
 
 }
